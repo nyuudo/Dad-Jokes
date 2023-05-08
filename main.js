9b3ff970b49d4aaa4b0daeb22ddc68436d1ddcbb
nyuudo <hello@nyuudo.com>
2023-05-08 21:21:02 +0200

    Exercise 3

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -11,7 +10,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 // DOM variables (main button, text area for jokes)
 const nextJoke = document.querySelector("#button");
 const jokeText = document.querySelector("#joke");
+const rating = document.querySelector(".rating");
 let currentJoke = "";
+let currentScore = 0;
 // Exercise 2 (data in DOM)
 // ASYNC function to get current Joke
 const getJoke = () => __awaiter(void 0, void 0, void 0, function* () {
@@ -28,6 +29,7 @@ const getJoke = () => __awaiter(void 0, void 0, void 0, function* () {
         currentJoke = data.joke;
         console.log(currentJoke);
         jokeText.innerHTML = `${currentJoke}`;
+        rating.classList.add("fadeIn");
         nextJoke.textContent = "NEXT JOKE";
         return currentJoke;
     }
@@ -35,7 +37,32 @@ const getJoke = () => __awaiter(void 0, void 0, void 0, function* () {
         console.error(error);
     }
 });
+// Exercise 3 (score Jokes)
+// Variables in order to retrieve joke scores
+const date = new Date();
+let currentDate = date.toISOString();
+const reportJokes = [];
+// Listener to retrieve scores as a number from DOM
+export const jokeStar = document.querySelectorAll('[id^="star"]');
+jokeStar.forEach((jokeStar) => {
+    const score = Number(jokeStar.id.substring(4));
+    jokeStar.addEventListener("click", () => (currentScore = score));
+});
+// Function to score any jokes and collect them into an array to be print in the console
+const jokeScore = (score) => {
+    // reportJokes variable to collect jokes evaluation
+    let report = {
+        joke: currentJoke,
+        score: score,
+        date: currentDate,
+    };
+    if (score !== 0) {
+        reportJokes.push(report);
+        console.log(reportJokes);
+    }
+};
 // Listeners for the main button
 nextJoke.addEventListener("click", () => {
     getJoke();
+    jokeScore(currentScore);
 });
--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 // DOM variables (main button, text area for jokes)
 const nextJoke = document.querySelector("#button") as HTMLButtonElement;
 const jokeText = document.querySelector("#joke") as HTMLElement;
+const rating = document.querySelector(".rating") as HTMLElement;
 
 let currentJoke: string = "";
+let currentScore: number = 0;
 // Exercise 2 (data in DOM)
 // ASYNC function to get current Joke
 const getJoke = async () => {
@@ -19,6 +21,7 @@ const getJoke = async () => {
     currentJoke = data.joke;
     console.log(currentJoke);
     jokeText.innerHTML = `${currentJoke}`;
+    rating.classList.add("fadeIn");
     nextJoke.textContent = "NEXT JOKE";
     return currentJoke;
   } catch (error) {
@@ -26,7 +29,35 @@ const getJoke = async () => {
   }
 };
 
+// Exercise 3 (score Jokes)
+// Variables in order to retrieve joke scores
+const date = new Date();
+let currentDate = date.toISOString();
+const reportJokes: { joke: string; score: number; date: string }[] = [];
+
+// Listener to retrieve scores as a number from DOM
+export const jokeStar = document.querySelectorAll('[id^="star"]');
+jokeStar.forEach((jokeStar) => {
+  const score = Number(jokeStar.id.substring(4));
+  jokeStar.addEventListener("click", () => (currentScore = score));
+});
+
+// Function to score any jokes and collect them into an array to be print in the console
+const jokeScore = (score: number) => {
+  // reportJokes variable to collect jokes evaluation
+  let report = {
+    joke: currentJoke,
+    score: score,
+    date: currentDate,
+  };
+  if (score !== 0) {
+    reportJokes.push(report);
+    console.log(reportJokes);
+  }
+};
+
 // Listeners for the main button
 nextJoke.addEventListener("click", () => {
   getJoke();
+  jokeScore(currentScore);
 });
